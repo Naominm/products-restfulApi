69c10dea7f143c0979489ee28936e901bb5d381a
Naominm <naomimbugua536@gmail.com>
2025-04-05 19:35:01 +0300

    Set the get all product route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-const app=express()
+const app = express();
 
+app.get("/products", (req, res) =>{
+    res.send("Getting all products")
+})
 
-let port= process.env.PORT || 3000;
+let port= process.env.PORT || 4000;
 
 app.listen(port,()=>{
-    console.log(`server running on port 3000`)
+    console.log(`server running on port 4000`)
 })
