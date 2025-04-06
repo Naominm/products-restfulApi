58807d61f465b819e488636de75acd9db83a880a
Naominm <naomimbugua536@gmail.com>
2025-04-06 09:44:39 +0300

    Remove routes code in the index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import productsRouter from "./routes/produtsRouter.js"
-import validateProducts from "./middleware/validateProducts.js";
+
 
 const app = express();
 const client=new PrismaClient();
@@ -9,13 +9,6 @@ const client=new PrismaClient();
 app.use(express.json());
 app.use('/products',productsRouter)
 
-app.get("/products", )
-
-app.get("/products/:productsId" ,)
-
-app.post("/products" ,[validateProducts] , )
-// app.patch("/products/:productsId", )
-// app.delete("/products/:productsId",)
 let port= process.env.PORT || 4000;
 
 app.listen(port,()=>{
