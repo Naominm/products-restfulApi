aa276eda4895764be18e3d51f05b49ab8fc748a9
Naominm <naomimbugua536@gmail.com>
2025-04-06 09:38:04 +0300

    Add Refactoring to the update route

--- index.js
+++ index.js
@@ -13,61 +13,8 @@ app.get("/products", )
 
 app.get("/products/:productsId" ,)
 
-app.post("/products" ,[validateProducts] , async (req,res)=>{
-const {productTitle, productDescription,unitsLeft,pricePerUnit,isOnOffer}=req.body
-try {
-    const newProduct= await client.products.create({
-        data:{
-            productTitle,
-            productDescription,
-            unitsLeft,
-            pricePerUnit,
-            isOnOffer
-        }
-    })
-    res.status(201).json({
-        status:"success",
-        message:"New Product added successfully",
-        data:newProduct
-    })
-} catch (e) {
-    res.status(500).json({
-        status:"Error",
-        message:"An error occurred"
-    })
-}
-})
-app.patch("/products/:productsId", async(req,res)=>{
-
-    const {productsId}=req.params
-
-    const{productTitle,productDescription,pricePerUnit,unitsLeft,isOnOffer}=req.body
-try {
-    const updatedProduct=await client.products.update({
-        where:{
-            id:productsId
-        },
-        data:{
-            productTitle:productTitle&&productTitle,
-            productDescription:productDescription && productDescription,
-            pricePerUnit:pricePerUnit&&pricePerUnit,
-            unitsLeft:unitsLeft&&unitsLeft,
-            isOnOffer:isOnOffer&&isOnOffer,
-        }
-    })
-    res.status(200).json({
-        status:"Success",
-        message:"Product Updated successfully",
-        data:updatedProduct
-    })
-} catch (e) {
-    res.status(500).json({
-        status:"Error",
-        message:"something went wrong"
-    })
-    
-}
-})
+app.post("/products" ,[validateProducts] , )
+// app.patch("/products/:productsId", )
 app.delete("/products/:productsId", async(req ,res)=>{
     const {productsId}=req.params;
     try {
@@ -83,7 +30,7 @@ app.delete("/products/:productsId", async(req ,res)=>{
     } catch (e) {
         res.status(500).json({
             status:"Error",
-            message:"Somethig went wrong"
+            message:"Something went wrong"
         })
         
     }
